34ca2d375a3bd097e0567b2884108753df9edbe1
jiangsifang <691887420@qq.com>
2020-12-09 09:27:53 +0800

    肖铭辉

---
+++ javaproject/src/com/nnxy/team5/xiaominghui_3.java
@@ -0,0 +1,24 @@
+package com.nnxy.team5;
+
+public class xiaominghui_3 {
+
+	private int id;
+	private String name;
+	
+	public int getid()
+	{
+		return id;
+	}
+	
+	public void setid(int id){
+		this.id=id;
+	}
+	
+	public String getname(){
+		return name;
+	}
+	
+	public void setname(String name){
+		this.name=name;
+	}
+}
