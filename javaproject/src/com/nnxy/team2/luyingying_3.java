4bfece76d6065a3a5a20b1499aa9448444fea9c3
jiangsifang <691887420@qq.com>
2020-12-09 09:56:34 +0800

    第一次提交

---
+++ javaproject/src/com/nnxy/team2/luyingying_3.java
@@ -0,0 +1,17 @@
+package com.nnxy.team2;
+
+public class luyingying_3 {
+	
+	private int name;
+	public int getName(){
+		return name;
+		
+	}
+	
+	public void setName(int name){
+		this.name=name;
+		
+	}
+}
+
+
