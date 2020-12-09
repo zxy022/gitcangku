3ec0a29f873b85c3d59a981add0e579189e524f1
jiangsifang <691887420@qq.com>
2020-12-09 09:59:26 +0800

    第一次提交

---
+++ javaproject/src/com/nnxy/team2/liaochang_3.java
@@ -0,0 +1,15 @@
+package com.nnxy.team2;
+
+public class liaochang_3 {
+
+	private int name;
+	public int getName(){
+		return name;
+		
+	}
+	
+	public void setName(int name){
+		this.name=name;
+		
+	}
+}
