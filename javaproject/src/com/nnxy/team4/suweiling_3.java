2c245c9d439562dc3a1eaecf95de3904f6c8d635
suweiling123 <2996379963@qq.com>
2020-12-09 09:22:52 +0800

    第一次提交2020-12-9

---
+++ javaproject/src/com/nnxy/team4/suweiling_3.java
@@ -0,0 +1,16 @@
+package com.nnxy.team4;
+
+public class suweiling_3 {
+	
+	private int id;
+	public int getid() {
+		
+		return id;
+	}
+	
+	public void setID(int id) {
+		
+		this.id = id;
+	}
+
+}
